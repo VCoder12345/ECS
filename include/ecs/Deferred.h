c9a4e210f5bf0fd60bde11780f63db37d4ea4253
VCoder12345 <valentinlungershausen@hotmail.com>
2026-09-14 10:59:42 +0200

    small structure changes

--- include/ecs/Deferred.h
+++ include/ecs/Deferred.h
@@ -1,12 +1,14 @@
 #pragma once
-#include <ecs/World.h>
 #include <ecs/Context.h>
+#include <ecs/World.h>
 
+// Call a function for each entity with the specified components, passing a
+// WorldCtxt to allow deferred modifications
+//Note that nested eachDeferred's will lead to undefined behaviour
 template <typename... Components, typename F>
 void eachDeferred(World &world, F &&func) {
-  WorldCtxt ctx;          
-  world.each<Components...>([&](Entity e, auto&... comps) {
-    func(ctx, e, comps...);
-  });
-  ctx.flush(world);                 
+  WorldCtxt ctx;
+  world.each<Components...>(
+      [&](Entity e, auto &...comps) { func(ctx, e, comps...); });
+  ctx.flush(world);
 }
